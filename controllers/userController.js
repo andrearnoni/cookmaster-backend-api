dc093c37cd82bd35f8b2502ee61f5aa7b9108024
André Arnoni <andre_arnoni@yahoo.com.br>
2021-10-07 02:08:19 -0300

    arrumado o catch do controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ const loginUser = async (req, res) => {
     if (result === null) return res.status(401).json(messages.INCORRECT_DATA);
 
     return res.status(200).json({ token });
-  } catch (error) {
+  } catch (err) {
     return res.status(500).json(messages.ERROR);
   }
 };
